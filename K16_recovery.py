0025e3bf8d4b990aa1e6f8a7e67ae43fcaf31e05
Blutch10 <80125434+Blutch10@users.noreply.github.com>
2022-03-15 14:49:03 +0100

    K16 recovery done

---
+++ K16_recovery.py
@@ -0,0 +1,56 @@
+from utils import *
+
+def K16_recovery():
+    '''
+    Recover the K16 subkey.
+
+    @Returns:
+        (string) : The K16 binary string (without 0b)
+    '''
+    # The possible key_parts found for each Sbox input
+    possible_keys_part = {
+        "S1" : [],
+        "S2" : [],
+        "S3" : [],
+        "S4" : [],
+        "S5" : [],
+        "S6" : [],
+        "S7" : [],
+        "S8" : []
+    }
+
+
+    R16, L16 = get_R16_L16(cipher_corr) # R16 and L16 registers from correct ciphertext
+    R15 = L16                           # According to equations R15 = L16
+    R15_expanded = expand(R15)          # Expansion of R15 to 48 bits
+
+    for faulted_cipher in cipher_fault:
+        R16_err, L16_err = get_R16_L16(faulted_cipher)  # R16 and L16 registers from faulted ciphertext
+        R15_err = L16_err
+        R15_err_expanded = expand(R15_err)
+
+        R16_xor_R16err = xor(R16, R16_err)              # R16 xor R16_err
+        revP_R16_xor_R16err = rev_perm(R16_xor_R16err)  # P^-1(R16 xor R16_err)
+
+        equations = []      # The equations we must solve
+        for i in range(0, 32, 4):
+            equations.append(revP_R16_xor_R16err[i:i+4])
+
+        for i in range(8):  # Exhaustive search on K16 subparts possible values
+            if equations[i] == '0000':
+                continue
+            sbox_name = 'S' + str(i + 1)
+            result = exhaustive_attack_sbox(sbox_name, R15_expanded[i*6:(i+1)*6], R15_err_expanded[i*6:(i+1)*6], equations[i])
+            if result != []:
+                possible_keys_part[sbox_name].append(result)
+        
+    for key in list(possible_keys_part.keys()):
+        possible_keys_part[key] = intersect(possible_keys_part[key])
+
+    # Recovery of K16
+    K16 = ''
+    for part in possible_keys_part.values():
+        K16 += part[0]
+    return K16
+
+K16_recovery()
--- utils.py
+++ utils.py
@@ -90,70 +90,74 @@ rev_P = [
     5, 27, 15, 21
 ]
 
-# DES Sbox S1
-S1 = [
-    [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
-    [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
-    [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
-    [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
-]
+# A dictionnary cntaining all Sboxes
+Sboxes = {
 
-# DES Sbox S2
-S2 = [
-    [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
-    [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
-    [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
-    [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
-]
+    # DES Sbox S1
+    "S1" : [
+        [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+        [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+        [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+        [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+    ],
 
-# DES Sbox S3
-S3 = [
-    [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
-    [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
-    [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
-    [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
-]
+    # DES Sbox S2
+    "S2" : [
+        [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+        [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+        [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+        [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
+    ],
 
-# DES Sbox S4
-S4 = [
-    [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
-    [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
-    [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
-    [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
-]
+    # DES Sbox S3
+    "S3" : [
+        [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+        [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+        [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+        [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
+    ],
 
-# DES Sbox S5
-S5 = [
-    [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
-    [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
-    [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
-    [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
-]
+    # DES Sbox S4
+    "S4" : [
+        [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+        [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+        [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+        [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
+    ],
 
-# DES Sbox S6
-S6 = [
-    [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
-    [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
-    [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
-    [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
-]
+    # DES Sbox S5
+    "S5" : [
+        [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+        [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+        [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+        [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
+    ],
 
-# DES Sbox S7
-S7 = [
-    [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
-    [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
-    [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
-    [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
-]
+    # DES Sbox S6
+    "S6" : [
+        [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+        [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+        [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+        [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
+    ],
 
-# DES Sbox S8
-S8 = [
-    [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
-    [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
-    [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
-    [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
-]
+    # DES Sbox S7
+    "S7" : [
+        [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+        [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+        [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+        [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
+    ],
 
+    # DES Sbox S8
+    "S8" : [
+        [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+        [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+        [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+        [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
+    ]
+
+}
 
 
 def expand(r):
@@ -161,7 +165,7 @@ def expand(r):
     DES expansion function contained in F function.
 
     Args:
-        r (hex) : The hexadecimal value of the right register.
+        r (string) : A string representing the binary value of R15.
     
     Raises:
         Exception : If the hexadecimal value given in parameter is not a 32 bits value.
@@ -169,13 +173,12 @@ def expand(r):
     Returns:
         string : A string representing the expanded value of r in binary (without the 0b).
     '''
-    if len(r) != 10:
-        raise Exception('The parameter value must be a 32 bits integer. Given one was a ' + str((len(r) - 2) * 4) + ' bits parameter.')
+    if len(r) != 32:
+        raise Exception('The parameter value must be a 32 bits integer. Given one was a ' + str(len(r)) + ' bits parameter.')
 
-    tmp = bin(int(r, 16))[2:].zfill(32) # Converts the hexadecimal parameter r in a string representing its binary value
     res = ''
     for bit in E:
-        res += tmp[bit - 1]
+        res += r[bit - 1]
     return res
 
 
@@ -197,29 +200,6 @@ def xor(a, b):
         raise Exception('The two parameters must have the same bit size. First was : ' + str(len(a)) + ' bits. Second was : ' + str(len(b)) + ' bits.')
 
     res = bin(int(a, 2) ^ int(b, 2))[2:].zfill(len(a))
-    return res
-
-
-def initial_permutation(cipher):
-    '''
-    Applies the initial permutation IP function to the ciphertext.
-
-    Args:
-        cipher (hex) : The ciphertext in hexadecimal.
-
-    Raises:
-        Exception : If the cipher parameter is not a 64 bits integer.
-
-    Returns:
-        string : A string representing the result of the permutation in binary.
-    '''
-    if len(cipher) != 18:
-        raise Exception('The ciphertext must be a 64 bits integer. Provided was a ' + str((len(cipher) - 2) * 4) + ' bits integer.')
-
-    res = ''
-    bin_cipher = bin(int(cipher, 16))[2:].zfill(48)
-    for bit in IP:
-        res += bin_cipher[bit - 1]
     return res
 
 
@@ -305,11 +285,11 @@ def get_R16_L16(cipher):
     if (len(cipher) - 2) * 4 != 64:
         raise Exception('The parameter must be a 64 bits hexadecimal value. Provided was a ' + str(len(cipher) - 2) * 4 + ' bits value.')
 
-    bin_cipher = bin(int(cipher, 16))[2:].zfill(32)
+    bin_cipher = bin(int(cipher, 16))[2:].zfill(64)
 
     rev = initial_perm(bin_cipher)
-    R16 = rev[:32]
     L16 = rev[32:64]
+    R16 = rev[:32]
     return R16, L16
 
 
@@ -335,13 +315,14 @@ def calculate_S(SBox, input):
     return bin(SBox[row][col])[2:].zfill(4)
 
 
-def exhaustive_attack_sbox(Sbox, input, expected):
+def exhaustive_attack_sbox(Sbox_name, R15, R15_fault, expected):
     '''
     Performs an exhaustive search of the possible K16 bits associated to the Sbox.
 
     Args:
-        Sbox(int[]) : The Sbox we are attacking.
-        input (string) : The 6 bits input of the Sbox (must correspond to E(R15))
+        Sbox_name (string) : The name of the Sbox we are attacking.
+        R15 (string) : The 6 bits input of the E(R15) value.
+        R15_fault (string) : The 6 bits input of the E(R15*) value.
         expected (string) : The expected output of the Sbox.
     
     Raises:
@@ -350,16 +331,39 @@ def exhaustive_attack_sbox(Sbox, input, expected):
     Returns:
         string[] : The list of possible K16 portions.
     '''
-    if len(input) != 6:
-        raise Exception('The parameter must be a 6 bits integer. Provided was a ' + str(len(input)) + ' bits value.')
+    if len(R15) != 6:
+        raise Exception('The parameter input_R15 must be a 6 bits integer. Provided was a ' + str(len(R15)) + ' bits value.')
+    if len(R15_fault) != 6:
+        raise Exception('The parameter input_R15_fault must be a 6 bits integer. Provided was a ' + str(len(R15_fault)) + ' bits value.')
     if len(expected) != 4:
         raise Exception('The parameter must be a 4 bits integer. Provided was a ' + str(len(input)) + ' bits value.')
     
     res = []
-    for i in range(16):
-        bin_i = bin(i)[2:].zfill(6)
-        out = calculate_S(Sbox, xor(input, bin_i))
-        if out == expected:
-            res.append(bin_i)
+    for key in range(64):
+        bin_key = bin(key)[2:].zfill(6)
+
+        out1 = calculate_S(Sboxes[Sbox_name], xor(R15, bin_key))
+        out2 = calculate_S(Sboxes[Sbox_name], xor(R15_fault, bin_key))
+        final_out = xor(out1, out2)
+        
+        if final_out == expected:
+            res.append(bin_key)
+    return res
+
+
+def intersect(lists):
+    '''
+    Returns the intersection of a lists contained in an other list.
+
+    Args:
+        lists ((string[])[]) : A list of string lists.
     
+    Returns:
+        list[string] : The intersection of all sublists.
+    '''
+    res = lists[0]
+    for i in range(1, len(lists)):
+        if lists[i] == []:
+            continue
+        res = list(set(res) & set(lists[i]))
     return res
